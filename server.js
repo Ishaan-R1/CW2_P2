9e3f80bd260bacc16ed4f5ed1a25bbc5eada2bc0
Ishaan-R1 <119463909+Ishaan-R1@users.noreply.github.com>
2023-01-30 15:46:11 +0000

    Updating server to work with AWS

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
-var express = require("express"); // Requires the Express module
-var path = require("path"); // Requires the Express module
+// Requires the relevant modules
+var express = require("express"); 
+var path = require("path"); 
 var morgan = require("morgan");
 const cors = require("cors");
 const { nextTick } = require("process");
@@ -8,68 +9,58 @@ let propertiesReader = require("properties-reader");
 let propertiesPath = path.resolve(__dirname, "conf/db.properties");
 let properties = propertiesReader(propertiesPath);
 let dbPprefix = properties.get("db.prefix");
-//URL-Encoding of User and PWD
-//for potential special characters
+//URL-Encoding of user and password from db.properties file
 let dbUsername = encodeURIComponent(properties.get("db.user"));
 let dbPwd = encodeURIComponent(properties.get("db.pwd"));
 let dbName = properties.get("db.dbName");
 let dbUrl = properties.get("db.dbUrl");
 let dbParams = properties.get("db.params");
 const uri = dbPprefix + dbUsername + ":" + dbPwd + dbUrl + dbParams;
-
+// Require MongoDB
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const { query } = require("express");
 const client = new MongoClient(uri, { serverApi: ServerApiVersion.v1 });
 let db = client.db(dbName);
 
-// Calls the express function to start a new Express application
+// Call express function to start application
 var app = express();
 app.use(morgan("short"));
-
+// Neatly format JSON
 app.set("json spaces", 3);
-
 app.use(cors());
-
 app.use(express.json());
 
+// Get name of collection once provided - either lessons or orders
 app.param("collectionName", function (req, res, next, collectionName) {
   req.collection = db.collection(collectionName);
   return next();
 });
-app.use(function (request, response, next) {
-  // middleware
-  console.log(
-    "recieved request with HTTP method " +
-      request.method +
-      " and url: " +
-      request.url
-  );
 
-  next();
-});
-
-// http://localhost:3000/collections/lessons
-// Get all lessons
+// Get all lessons - collectionName specficed in fetch in app.js
 app.get("/collections/:collectionName", function (req, res, next) {
-  req.collection.find({}).toArray(function (err, results) {
+  // Get all the results from the specified collection
+  req.collection.find({}).toArray(function (err, lessonResults) {
     if (err) {
       return next(err);
     }
-    res.send(results);
+    // Display results
+    res.send(lessonResults);
   });
 });
 // Send new order to DB
 app.post("/collections/:collectionName", function (req, res, next) {
-  req.collection.insertOne(req.body, function (err, results) {
+  // Insert new document in specified collection and then get results
+  req.collection.insertOne(req.body, function (err, orderResults) {
     if (err) {
       return next(err);
     }
-    res.send(results);
+    // Display results
+    res.send(orderResults);
   });
 });
 // Update spaces in lesson collection 
 app.put("/collections/:collectionName/:id", function (req, res, next) {
-  // TODO: Validate req.body
+  // Update element based on provided ID
   req.collection.updateOne(
     { _id: new ObjectId(req.params.id) },
     { $set: req.body },
@@ -78,6 +69,7 @@ app.put("/collections/:collectionName/:id", function (req, res, next) {
       if (err) {
         return next(err);
       } else {
+        // Make sure there is only one match to be successful
         res.send(
           result.matchedCount === 1 ? { msg: "success" } : { msg: "error" }
         );
@@ -85,62 +77,49 @@ app.put("/collections/:collectionName/:id", function (req, res, next) {
     }
   );
 });
-//======================================================================================================
 // Search Functionality - Get route to retrieve user input and return search result from DB
 app.get("/search/:collectionName/:subject", function (req, res, next) {
+  // Create query to check subject mathces a subject stored in lesson collection
   let query = { subject: { $regex: req.params.subject } };
   console.log(typeof req.params.subject);
+  // Get all the matching results from the specified collection
   req.collection.find(query).toArray(function (err, results) {
     if (err) {
       return next(err);
     }
-    // COULD WORK BUT NEED TO FETCH INSTANLY - NOT ON BUTTON CLICK
-    // res.send(results.filter((matches) => {
-    //   return (
-    //     matches.subject.includes(query)
-    //   );
-    // }));
     console.log("===================================");
+    // Return the search results
     res.send(results);
-    console.log(typeof results);
-
+    console.log(results);
     console.log("===================================");
   });
 });
 
-//======================================================================================================
-app.use(function (req, res, next) {
-  console.log("Incoming request: " + req.url);
-  next();
-});
-// Middleware to outpur all requests to console
+// Middleware to output requests to server 
 app.use(function (request, response, next) {
   console.log(
-    "recieved request with HTTP method " +
-      request.method +
-      " and url: " +
-      request.url
+    "Request recieved using " + request.method + " request" +
+      " with URL: " + request.url
   );
-
   next();
 });
 // Static file middleware to return lesson images
 var imagesPath = path.join(__dirname, "images");
 app.use("/images", express.static(imagesPath));
-// GET route to display message if user accesses server without specifying path
-app.get("/", function (req, res, next) {
-  res.send("Welcome to After School Web Page");
-});
+
 // Error message if image file does not exist
 app.use(function (request, response) {
   response.status(404);
   response.send("Image File Does Not Exist");
 });
 
-// app.listen(3000, function () {
-//   console.log("App started on port 3000");
-// });
+// GET route to display message if user accesses server without specifying path
+app.get("/", function (req, res, next) {
+  res.send("Welcome to After School Web Page");
+});
+
+// Allow AWS to choose port or choose port 3000
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log("App started on port: "+port);
+  console.log("App started on port: " + port);
 });
